39dd06c9277f30bfdce594a86ce71f52e28e5dc8
esdx245 <esdx245@khu.ac.kr>
2022-06-13 11:56:32 +0900

    11000.py

---
+++ Greedy/11000.py
@@ -0,0 +1,21 @@
+import heapq
+case = int(input())
+q = []
+
+for i in range(case):
+    start, end = map(int, input().split())
+    q.append([start, end])
+
+q.sort()
+
+classs = []
+heapq.heappush(classs, q[0][1])
+
+for i in range(1, case):
+    if q[i][0] < classs[0]:
+        heapq.heappush(classs, q[i][1])
+    else:
+        heapq.heappop(classs)
+        heapq.heappush(classs, q[i][1])
+    print(classs)
+print(len(classs))
