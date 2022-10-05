a11bc64817b3aa0118577fd5495ca33df391e955
esdx245 <esdx245@khu.ac.kr>
2022-10-05 17:51:49 +0900

    changed 2270.py

--- Greedy/2750.py
+++ Greedy/2750.py
@@ -1,18 +1,7 @@
 n = int(input())
-lista = [0, 1001]
+lista = []
 for i in range(n):
-    temp = int(input())
-    mid = len(lista)//2
-    while 1:
-        if lista[mid] < temp < lista[mid+1]:
-            templist = lista[:mid+1]
-            templist.append(temp)
-            lista = templist + lista[mid+1:]
-            break
-        elif lista[mid] > temp:
-            mid = mid//2
-        else:
-            mid += mid // 2
-
-for _ in range(1, n+1):
-    print(lista[_])
+    lista.append(int(input()))
+lista.sort()
+for i in range(n):
+    print(lista[i])
